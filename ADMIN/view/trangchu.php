d36f88a5f101a9f9a5a4a6502b94eb8bcd51befc
phanngocson0407 <sonfpt149@gmail.com>
2022-12-21 21:07:39 +0700

    dasdsada

--- ADMIN/view/trangchu.php
+++ ADMIN/view/trangchu.php
@@ -42,15 +42,14 @@
         <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
       </li>
     </ul>
-
+    
     <!-- Right navbar links -->
     <ul class="navbar-nav ml-auto">
-      <li class="nav-item">
-        <a class="nav-link" data-widget="fullscreen" href="#" role="button">
-          <i class="fas fa-expand-arrows-alt"></i>
-        </a>
-      </li>
-      
+        <li class="nav-icon" >
+          <a href="./index.php"><i class="fa fa-user" style="font-size:30px;color:black"></i></a>
+          </a>
+        </li>
+    </ul>
   </nav>
   <!-- /.navbar -->
 
